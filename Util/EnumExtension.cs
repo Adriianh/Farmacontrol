94a3c36be7bdb0a3127014b80c802bdb071b4b9c
Adrián Fúnez <adriianh@proton.me>
2026-04-24 00:45:17 -0600

    feat: add EnumExtension with GetDescription method for enum descriptions

---
+++ Util/EnumExtension.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Farmacontrol.Util
+{
+    public static class EnumExtension
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            if (field == null) return "";
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+            
+            return attribute == null ? value.ToString() : attribute.Description;
+
+        }
+    }
+}
