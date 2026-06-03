38543a87438df950ab0842f0483d591e1a598ecd
Adrián Fúnez <adriianh@proton.me>
2026-06-03 16:10:04 -0600

    feat(db): implement AddStatusToPurchase migration

---
+++ Farmacontrol.Core/Migrations/20260603214908_AddStatusToPurchase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farmacontrol.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToPurchase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Purchases",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Purchases");
+        }
+    }
+}
