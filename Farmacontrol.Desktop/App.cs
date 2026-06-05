0d5eed8a0e233c6885baeac630f46124b6de9b5a
Adrián Fúnez <adriianh@proton.me>
2026-06-04 19:30:51 -0600

    feat(desktop): add configuration and state services

--- Farmacontrol.Desktop/App.cs
+++ Farmacontrol.Desktop/App.cs
@@ -5,6 +5,7 @@ using Farmacontrol.Desktop.States;
 using Farmacontrol.Desktop.Views;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace Farmacontrol.Desktop;
 
@@ -17,23 +18,38 @@ public static class Program
     {
         var serviceProvider = ConfigureServices();
         ServiceProvider = serviceProvider;
-        
+
         await ApplyMigrationsAsync(serviceProvider);
         BuildApp(args, serviceProvider);
     }
 
-    private static ServiceProvider ConfigureServices() => new ServiceCollection()
-        .AddFarmacontrolCore()
-        .AddTransient<InventoryState>()
-        .AddTransient<SupplierState>()
-        .AddTransient<SearchProductState>()
-        .AddTransient<PendingOrdersState>()
-        .AddTransient<SaleState>()
-        .AddTransient<VoidSaleState>()
-        .AddTransient<SalesReportState>()
-        .AddTransient<SalesHistoryState>()
-        .AddSingleton<ProductState>()
-        .BuildServiceProvider();
+    private static ServiceProvider ConfigureServices()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        return new ServiceCollection()
+            .AddSingleton<IConfiguration>(configuration)
+            .AddFarmacontrolCore()
+            .AddTransient<InventoryState>()
+            .AddTransient<SupplierState>()
+            .AddTransient<SearchProductState>()
+            .AddTransient<PendingOrdersState>()
+            .AddTransient<SaleState>()
+            .AddTransient<VoidSaleState>()
+            .AddTransient<SalesReportState>()
+            .AddTransient<SalesHistoryState>()
+            .AddTransient<AlertsState>()
+            .AddTransient<AlertsHistoryState>()
+            .AddTransient<UsersState>()
+            .AddTransient<LoginState>()
+            .AddSingleton<RootState>()
+            .AddSingleton<MainView.State>()
+            .AddTransient<DashboardState>()
+            .AddSingleton<ProductState>()
+            .BuildServiceProvider();
+    }
 
     private static async Task ApplyMigrationsAsync(IServiceProvider serviceProvider)
     {
@@ -70,7 +86,7 @@ public static class Program
             .Height(800)
             .MinWidth(1000)
             .MinHeight(600)
-            .Content(ViewFactory.Create<MainView>());
+            .Content(ViewFactory.Create<RootView>());
 
         lifetime.Start(args);
     }
